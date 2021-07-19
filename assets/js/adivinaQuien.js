4731783666112fd4c2dcda1c66f3ce60e726b7fb
sanchezcode <sanchezcode7@gmail.com>
2021-07-18 22:03:15 -0500

    Update 18/07/21

---
+++ assets/js/adivinaQuien.js
@@ -0,0 +1,352 @@
+//Se crea la variable nombre
+var nombre;
+//Function para el nombre de: Yennifer Revelo
+function nombre1() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "yennifer revelo") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Andrea Rodriguez
+function nombre2() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "andrea rodriguez" || nombre == "andrea rodríguez") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Margoth Blanco
+function nombre3() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "margoth blanco") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Catherine Bonilla
+function nombre4() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "catherine bonilla") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Camila Restrepo
+function nombre5() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "camila restrepo") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Jeidy Valbuena
+function nombre6() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "jeidy valbuena") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Juanita Pardo
+function nombre7() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "juanita pardo") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Tatiana Bolivar
+function nombre8() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "tatiana bolivar") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Nataly Rodriguez
+function nombre9() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "nataly rodriguez" || nombre == "nataly rodríguez") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Adriana Moreno
+function nombre10() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "adriana moreno") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Karen Triana
+function nombre11() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "karen triana") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Vivian Hong
+function nombre12() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "vivian hong") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Laura Lopez
+function nombre13() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "laura lopez") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Laury Saiz
+function nombre14() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "laury saiz") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Carolina Porras
+function nombre15() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "carolina porras") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Rosa Piñeros
+function nombre16() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "rosa piñeros") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Adriana Castañeda
+function nombre17() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "adriana castañeda") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Lorena Trujillo
+function nombre18() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "lorena trujillo") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Ximena Espindola
+function nombre19() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "ximena espindola") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Karen Majey
+function nombre20() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "karen majey") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Vanessa Sanchez
+function nombre21() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "vanessa sanchez") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Laura Camelo
+function nombre22() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "laura camelo") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Erika Soler
+function nombre23() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "erika soler") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Melissa Rincon
+function nombre24() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "melissa rincon" || nombre == "melissa rincón") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Gloria Gamez
+function nombre25() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "Gloria Gamez") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Malka Avendaño
+function nombre26() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "malka avendaño") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Diana Muñoz
+function nombre27() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "diana muñoz") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Maria Fernanda Russi
+function nombre28() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "maria fernanda russi") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Ana Castillo
+function nombre29() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "ana castillo") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Ivonne Zuñiga
+function nombre30() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "ivonne zuñiga") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Tatiana Bello
+function nombre31() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "tatiana bello") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Johanna Martinez
+function nombre32() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "johanna martinez") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Julieth Ruiz
+function nombre33() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "julieth ruiz") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Nanffy Barbosa
+function nombre34() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "nanffy barbosa") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
+//Function para el nombre de: Ánsalo
+function nombre35() {
+  nombre = prompt("¿Cual es el nombre y apellido de la compañera?");
+  nombre = nombre.toLowerCase();
+  if (nombre == "andres sanchez" || nombre == "andrés sánchez") {
+    alert("El nombre es correcto!");
+  } else {
+    alert("¬¬ no es");
+  }
+}
